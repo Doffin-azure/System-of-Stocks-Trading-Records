4f578f634cf55b024c0b4cecfe7644c3ae5b2f4a
Doffin-azure <2043716678@qq.com>
2024-11-24 14:52:39 +0800

    first try

---
+++ demo/hadoop-mapreduce-app/src/main/java/io/github/Doffin_azure/hadoop_mapreduce_app/HadoopMapreduceAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.Doffin_azure.hadoop_mapreduce_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopMapreduceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadoopMapreduceAppApplication.class, args);
+	}
+
+}
